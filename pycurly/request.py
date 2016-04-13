2a2c140466ea5d83cfbdd9a9feed3b893dfc1b2b
Umair Khan <umair.waheed@gmail.com>
2016-04-13 16:21:10 +0500

    add support for sock5 proxy and improve Response naming

--- pycurly/request.py
+++ pycurly/request.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+from urllib import parse
 
 import pycurl
 
@@ -6,11 +7,17 @@ from . import response
 
 
 class Request():
-    def __init__(self, url):
+    proxy_type = {
+        'socks5': pycurl.PROXYTYPE_SOCKS5_HOSTNAME,
+    }
+
+    def __init__(self, url, proxy=None):
         self.databuffer = BytesIO()
         c = self.curl = pycurl.Curl()
         c.setopt(c.URL, url)
         c.setopt(c.WRITEDATA, self.databuffer)
+        if proxy is not None:
+            self.add_proxy(proxy)
 
     def __del__(self):
         self.curl.close()
@@ -19,7 +26,13 @@ class Request():
         self.curl.perform()
         return response.Response(self.curl, self.databuffer)
 
+    def add_proxy(self, proxy_url):
+        token = parse.urlparse(proxy_url)
+        self.curl.setopt(pycurl.PROXY, token.hostname)
+        self.curl.setopt(pycurl.PROXYPORT, token.port or 80)
+        self.curl.setopt(pycurl.PROXYTYPE, self.proxy_type[token.scheme])
 
-def get(url):
-    request = Request(url)
+
+def get(url, **kwargs):
+    request = Request(url, **kwargs)
     return request.get()
--- pycurly/response.py
+++ pycurly/response.py
@@ -4,4 +4,9 @@ import pycurl
 class Response():
     def __init__(self, curl, databuffer):
         self.status_code = curl.getinfo(pycurl.HTTP_CODE)
-        self.data = databuffer.getvalue().decode('iso-8859-1')
+        self.content = databuffer.getvalue()
+        self.encoding = 'iso-8859-1'
+
+    @property
+    def text(self):
+        return self.content.decode(self.encoding)
